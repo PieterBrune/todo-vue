627c03923a7629af188caa4da96083880ffa38cc
pieter brune <pebrune@gmail.com>
2022-06-03 16:30:40 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app" class="container">
+    <img src="./assets/logo.png" class="logo">
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TodoList
+  }
+}
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 20px;
+}
+
+.logo {
+  display: block;
+  height: 75px;
+  margin: 20px auto;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+    <div v-for="(todo, index) in todos" :key="todo.id">
+      <li class="todo-item">
+        {{ todo.title }}
+        <button @click="removeTodo(index)" class="remove-item">&times;</button>
+      </li>
+    </div>
+  </div>
+</template>
+
+<script>
+
+let id = 0
+
+export default {
+  name: 'todo-list',
+  data () {
+    return {
+      newTodo: '',
+      todos: [
+        {
+          id: id++,
+          title: 'Finish learning Vue',
+          completed: false
+        },
+        {
+          id: id++,
+          title: 'Finish learning Go',
+          completed: false
+        },
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+
+      if (this.newTodo.trim().length === 0) {
+        return
+      }
+
+      this.todos.push({id: id++, title: this.newTodo, completed: false })
+
+      this.newTodo = ''
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+  }
+
+  .todo-input:focus {
+    outline: 0;
+  }
+
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .remove-item {
+    cursor: pointer;
+  }
+
+  .remove-item:hover {
+    color: red;
+  }
+</style>
