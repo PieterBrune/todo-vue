2e429f0664927bcf9fe23baf528b075148be87cf
pieter brune <pebrune@gmail.com>
2022-06-13 12:49:07 +0800

    Added clear completed function and button

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -26,8 +26,12 @@
         <button :class="{ active: filter === 'active' }" @click="filter = 'active'">Active</button>
         <button :class="{ active: filter === 'completed' }" @click="filter = 'completed'">Completed</button>
       </div>
-    </div>
 
+      <div>
+        <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
+      </div>
+
+    </div>
 
   </div>
 </template>
@@ -75,6 +79,9 @@ export default {
         return this.todos.filter(todo => todo.completed)
       }
       return this.todos
+    },
+    showClearCompletedButton() {
+      return this.todos.filter(todo => todo.completed).length > 0
     }
   },
   directives: {
@@ -114,6 +121,9 @@ export default {
     },
     checkAllTodos() {
       this.todos.forEach((todo) => todo.completed = event.target.checked)
+    },
+    clearCompleted() {
+      this.todos = this.todos.filter(todo => !todo.completed)
     }
   }
 }
